9dae3c5f949c855ff983ece5f1bca104b8041790
Will <willjcGA@outlook.com>
2022-09-13 21:28:48 +0800

    feat: added addditional details to sign in form, started styling

--- src/components/signup-form/SignUpForm.jsx
+++ src/components/signup-form/SignUpForm.jsx
@@ -1,68 +1,104 @@
-import TextField from  "@mui/material/TextField";
-import Box from  "@mui/material/Box";
-import Grid from  "@mui/material/Grid";
+import TextField from "@mui/material/TextField";
+import Box from "@mui/material/Box";
+import Typography from "@mui/material/Typography";
 import Button from "@mui/material/Button";
+import "./SignUpForm.css";
 
 function LoginForm(props) {
-    return (
-        <Box sx={{boxShadow: 7, padding: 3}}>
-            <form method="post" action="/register" id="registration-form">
-                <Grid container 
-                    spacing={2}
-                    direction="column"
-                    justifyContent="center"
-                    alignItems="center"
-                >
-                    <Grid item>
-                        <label htmlFor="username">Username</label><br />
-                        <TextField required hiddenLabel
-                            id="username"
-                            defaultValue="Ex. Melody"
-                            variant="filled"
-                            size="small"
-                            form="registration-form"
-                        />
-                    </Grid>
-                    <Grid item>
-                        <label htmlFor="email">Email</label><br />
-                        <TextField required hiddenLabel
-                            id="email"
-                            defaultValue="Ex. Melody@email.com"
-                            variant="filled"
-                            size="small"
-                        />
-                    </Grid>
-                    <Grid item>
-                        <label htmlFor="password">Password</label><br />
-                        <TextField required hiddenLabel
-                            id="password"
-                            type="password"
-                            variant="filled"
-                            size="small"
-                            />
-                    </Grid>
-                    <Grid item>
-                        <label htmlFor="confirm-password">Confirm Password</label><br />
-                        <TextField required hiddenLabel
-                            id="confirm-password"
-                            type="password"
-                            variant="filled"
-                            size="small"
-                        />  
-                    </Grid>
-                    <Grid item>
-                        <Button variant="outlined">Sign Up</Button>
-                    </Grid>
-                    <Grid item>
-                        <h3>Already have an account?</h3>
-                    </Grid>
-                    <Grid item>
-                        <Button variant="contained">Login</Button>
-                    </Grid>
-                </Grid>
-            </form>
+  return (
+    <Box
+      sx={{
+        boxShadow: 7,
+        padding: 3,
+        display: "flex",
+        flexDirection: "column",
+        alignItems: "center",
+        justifyContent: "center",
+        backgroundColor: "var(--color1)"
+      }}
+    >
+      <form method="post" action="/register" id="registration-form">
+        <Typography variant="subtitle1" gutterBottom>
+          Username
+        </Typography>
+        <TextField
+          required
+          hiddenLabel
+          fullWidth
+          id="username"
+          defaultValue="Ex. Melody"
+          variant="filled"
+          size="small"
+          form="registration-form"
+          sx={{ marginBottom: 2 }}
+          className="input-text"
+        />
+        <Typography variant="subtitle1" gutterBottom>
+          Email
+        </Typography>
+        <TextField
+          required
+          hiddenLabel
+          fullWidth
+          id="email"
+          defaultValue="Ex. Melody@email.com"
+          variant="filled"
+          size="small"
+          sx={{ marginBottom: 2 }}
+          className="input-text"
+        />
+        <Typography variant="subtitle1" gutterBottom>
+          Password
+        </Typography>
+        <TextField
+          required
+          hiddenLabel
+          fullWidth
+          id="password"
+          type="password"
+          variant="filled"
+          size="small"
+          sx={{ marginBottom: 2 }}
+          className="input-text"
+        />
+        <Typography variant="subtitle1" gutterBottom>
+          Confirm Password
+        </Typography>
+        <TextField
+          required
+          hiddenLabel
+          fullWidth
+          id="confirm-password"
+          type="password"
+          variant="filled"
+          size="small"
+          sx={{ marginBottom: 2 }}
+          className="input-text"
+        />
+        <Box textAlign={"center"}>
+          <Button variant="outlined" size="small" id="sign-up-button" fullWidth>
+            Sign Up
+          </Button>
         </Box>
-    );
+        <Box textAlign={"center"} mt={2} mb={2}>
+          <Typography
+            variant="subtitle1"
+            align={"center"}
+            display={"inline"}
+            paddingX={1}
+            gutterBottom
+          >
+            Already have an account?
+          </Typography>
+        </Box>
+        <Box textAlign={"center"}>
+          <Button variant="contained" size="small" fullWidth id="login-button">
+            Login
+          </Button>
+        </Box>
+      </form>
+    </Box>
+  );
 }
 
-export default LoginForm
+export default LoginForm;
