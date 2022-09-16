e6c3247e118411b73ada8e24a3b8bdf0ff25710a
MUI LE <tanmui1992@gmail.com>
2022-09-16 17:33:06 +0800

    feat(menubar): update AvatarComponent

--- src/components/site-header/MenuBar.jsx
+++ src/components/site-header/MenuBar.jsx
@@ -4,7 +4,7 @@ import MenuItem from "@mui/material/MenuItem";
 import Typography from "@mui/material/Typography";
 import Tooltip from "@mui/material/Tooltip";
 import IconButton from "@mui/material/IconButton";
-import Avatar from "@mui/material/Avatar";
+import AvatarComponent from "../avatar/Avatar";
 import { Link } from "react-router-dom";
 
 function MenuBar(props) {
@@ -22,7 +22,10 @@ function MenuBar(props) {
     <div>
       <Tooltip title="Open settings">
         <IconButton onClick={handleOpenUserMenu} sx={{ p: 0 }}>
-          <Avatar alt="Remy Sharp" src="/static/images/avatar/2.jpg" />
+          <AvatarComponent
+            imgAlt="profileOwner"
+            src="/static/images/avatar/2.jpg"
+          />
         </IconButton>
       </Tooltip>
       <Menu
