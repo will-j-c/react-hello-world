7a810b571dbcbdbd5fe3cac2380e8bdb5b2d4296
Will <willjcGA@outlook.com>
2022-09-17 20:31:24 +0800

    feat: added start logic for post comment

--- src/components/comments/CommentPanel.jsx
+++ src/components/comments/CommentPanel.jsx
@@ -1,16 +1,36 @@
 import Grid from "@mui/material/Unstable_Grid2";
 import Comment from "./Comment";
+import CommentAddField from "./CommentAddField";
+
 
 function CommentPanel(props) {
   const comments = props.comments;
   const commentsToShow = comments.map((comment, idx) => {
     return (
-      <Grid padding={1} width={1} item key={idx} >
-        <Comment  comment={comment}/>
+      <Grid padding={1} width={1} item key={idx}>
+        <Comment comment={comment} />
       </Grid>
     );
   });
-  return <Grid flexDirection={"column"} alignItems={"center"} width={1} sx={{border: "solid 1px var(--color3)", height: "100%", backgroundColor: "var(--color2)"}} height={1} container>{commentsToShow}</Grid>;
+  return (
+    <Grid
+      flexDirection={"column"}
+      alignItems={"center"}
+      width={1}
+      sx={{
+        border: "solid 1px var(--color3)",
+        height: "100%",
+        backgroundColor: "var(--color2)",
+      }}
+      height={1}
+      container
+    >
+      {commentsToShow}
+      <Grid width={1} paddingX={1} alignSelf={"flex-end"} item>
+        <CommentAddField postComment={props.postComment} />
+      </Grid>
+    </Grid>
+  );
 }
 
 export default CommentPanel;
--- src/components/project-show-grid/ProjectShowGrid.jsx
+++ src/components/project-show-grid/ProjectShowGrid.jsx
@@ -1,27 +1,31 @@
 import Grid from "@mui/material/Unstable_Grid2";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
-import { useLocation } from "react-router-dom";
-import { useState, useEffect } from "react";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect, useContext } from "react";
+import AuthContext from "../../context/AuthProvider";
 import AvatarComponent from "../avatar/Avatar";
 import ProjectAboutPanel from "../project-about-panel/ProjectAboutPanel";
 import ProjectContributorsPanel from "../project-contributors-panel/ProjectContributorsPanel";
 import ShowTabs from "../show-tabs/ShowTabs";
-import './ProjectShowGrid.css'
-import axios from "../../api/axios"
+import "./ProjectShowGrid.css";
+import axios from "../../api/axios";
+import useAxiosPrivate from "../../hooks/useAxiosPrivate"
 import CommentPanel from "../comments/CommentPanel";
 
-  const baseProjectLogo =
-    "https://cdn.pixabay.com/photo/2017/01/31/20/53/robot-2027195_960_720.png";
+const baseProjectLogo =
+  "https://cdn.pixabay.com/photo/2017/01/31/20/53/robot-2027195_960_720.png";
 
 function ProjectShowGrid(props) {
   const location = useLocation();
+  const navigate = useNavigate();
+  const axiosPrivate = useAxiosPrivate();
+  const { auth } = useContext(AuthContext);
   const slug = location.pathname.split("/").pop();
   const [project, setProject] = useState(null);
   const [contributors, setContributors] = useState(null);
   const [creator, setCreator] = useState(null);
   const [comments, setComments] = useState(null);
-
 
   useEffect(() => {
     axios.get(`/projects/${slug}`).then(
@@ -30,17 +34,17 @@ function ProjectShowGrid(props) {
         setContributors(response.data.jobs);
         setCreator(response.data.createdBy);
       },
-      (error) => {
-        
-      }
+      (error) => {}
     );
 
-    axios.get(`/comments/${slug}`).then((response) => {
-      setComments(response.data);
-    })
+    axios.get(`/comments/${slug}`).then(
+      (response) => {
+        setComments(response.data);
+      },
+      (error) => {}
+    );
     // eslint-disable-next-line react-hooks/exhaustive-deps
   }, []);
-
 
   // Logic for handling tabs
   useEffect(() => {
@@ -54,12 +58,36 @@ function ProjectShowGrid(props) {
     setTabValue(newTabValue);
     return newTabValue === "1"
       ? setPanel(<ProjectAboutPanel project={project} />)
-      : setPanel(<ProjectContributorsPanel creator={creator} contributors={contributors} />);
+      : setPanel(
+          <ProjectContributorsPanel
+            creator={creator}
+            contributors={contributors}
+          />
+        );
+  };
+
+  // Logic for posting a comment
+  const postComment = (content) => {
+    // If not authorised, send to login page
+    if (!auth.username) {
+      return navigate("/login");
+    }
+    console.log(auth)
+    // If authorised, send post request for new comment
+    axiosPrivate.post(`/comments/${slug}`, { content }).then(
+      (response) => {
+        console.log('Response', response);
+      },
+      (error) => {
+        console.log('Error', error);
+      }
+    );
+    // Update comments with a fetch request
   };
 
   return project ? (
     <>
-      <Box display={"flex"} marginTop={4} >
+      <Box display={"flex"} marginTop={4}>
         <AvatarComponent
           imgAlt={project.title}
           imgUrl={project.logo_url || baseProjectLogo}
@@ -85,26 +113,32 @@ function ProjectShowGrid(props) {
         columns={{ xs: 1, md: 12 }}
         justifyContent="space-between"
         marginTop={4}
-        sx={{height: "70vh"}}
       >
-        <Grid md={7} alignSelf={"flex-start"} paddingTop={0} height={1} sx={{height: "100%"}} item>
+        <Grid
+          md={8}
+          alignSelf={"flex-start"}
+          paddingTop={0}
+          height={1}
+          sx={{ height: "100%" }}
+          item
+        >
           <Box
             sx={{
               border: "solid 1px var(--color3)",
               backgroundColor: "var(--color2)",
-              height: "100%"
+              height: "100%",
             }}
             paddingX={4}
             paddingBottom={4}
             id="panel-box"
             height={1}
           >
-            <ShowTabs tabValue={tabValue} handleTabChange={handleTabChange}/>
-              {project ? panel : ""}
+            <ShowTabs tabValue={tabValue} handleTabChange={handleTabChange} />
+            {project ? panel : ""}
           </Box>
         </Grid>
-        <Grid md={5} sx={{height: "100%"}} item>
-          <CommentPanel comments={comments}/>
+        <Grid md={4} sx={{ height: "100%" }} item>
+          <CommentPanel comments={comments} postComment={postComment} />
         </Grid>
       </Grid>
     </>
