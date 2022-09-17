bd7b14d9f69083df709b346b8b64cf72f9a20a0e
Zoe Nguyen <nganguyen0703@gmail.com>
2022-09-17 17:12:41 +0800

    chore: create files

---
+++ src/components/user-card/UserCard.jsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import Card from "@mui/material/Card";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import Typography from "@mui/material/Typography";
+import Box from "@mui/material/Box";
+import AvatarComponent from "../avatar/Avatar";
+import { Link as RouterLink } from "react-router-dom";
+import Button from "../buttons/Button";
+
+export default function UserCard(props) {
+  return (
+    <div>
+      
+    </div>
+  )
+}
