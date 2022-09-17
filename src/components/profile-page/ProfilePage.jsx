f7f32c7552236b0ffb6094749d442177c4c6a8db
MUI LE <tanmui1992@gmail.com>
2022-09-17 10:45:24 +0800

    chore: update

--- src/components/profile-page/ProfilePage.jsx
+++ src/components/profile-page/ProfilePage.jsx
@@ -1,8 +1,17 @@
 import React, { useEffect, useState } from "react";
-import { Link } from "react-router-dom";
-import { useTheme } from "@mui/material";
 import { useParams, useNavigate } from "react-router-dom";
+import { Link } from "react-router-dom";
+import Container from "@mui/material/Container";
 import axios from "axios";
+import Typography from "@mui/material/Typography";
+import Grid from "@mui/material/Grid";
+import Box from "@mui/material/Box";
+import Avatar from "@mui/material/Avatar";
+import Card from "@mui/material/Card";
+import CardActionArea from "@mui/material/CardActionArea";
+import CardMedia from "@mui/material/CardMedia";
+import CardContent from "@mui/material/CardContent";
+import AvatarComponent from "../avatar/Avatar";
 
 function ProfilePage(props) {
   const params = useParams();
@@ -15,8 +24,29 @@ function ProfilePage(props) {
         setProfile(response.data);
       });
   }, []);
-  console.log(profile);
-  return <>{/* {profile ? "hehe" : ""} */}</>;
+  console.log("profile content:", profile);
+  return (
+    <>
+      <Container sx={{ width: 900 }}>
+        <Grid container>
+          <Grid item xs={3}>
+            <Box>
+              <img
+                src={profile?.profile_pic_url}
+                width="100"
+                className="profileAvatar"
+              />
+            </Box>
+          </Grid>
+          <Grid item xs={6}>
+            <Typography variant="h3" component="h3" color="text.secondary">
+              Explore the world
+            </Typography>
+          </Grid>
+        </Grid>
+      </Container>
+    </>
+  );
 }
 
 export default ProfilePage;
