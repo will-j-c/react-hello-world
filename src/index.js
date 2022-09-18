83245e0603665b2b3f964a79e72ee922c8860767
Zoe Nguyen <nganguyen0703@gmail.com>
2022-09-18 21:21:57 +0800

    feat: add contributor show route in index.js

--- src/index.js
+++ src/index.js
@@ -16,6 +16,7 @@ import HomeGrid from "./components/home-grid/HomeGrid";
 import ProjectShowGrid from "./components/project-show-grid/ProjectShowGrid";
 import TitleHomepage from "./components/title-homepage/TitleHomepage"
 import ProjectIndexGrid from "./components/project-index-grid/ProjectIndexGrid";
+import ContributorShow from "./components/contributor-show/ContributorShow";
 
 const theme = createTheme({
   components: {
@@ -64,6 +65,9 @@ root.render(
                 <Route path="contributors" element={<ContributorIndexGrid />} />
                 <Route path="projects">
                   <Route path=":slug" element={<ProjectShowGrid />}/>
+                </Route>
+                <Route path="contributors">
+                  <Route path=":id" element={<ContributorShow />}/>
                 </Route>
                 <Route path="" element={<HomeGrid />} />
                 <Route
