fd8dfd9f1cca9b5e63bea0db070283ebb9eb6c7a
MUI LE <tanmui1992@gmail.com>
2022-09-19 01:44:10 +0800

    fix: add default photo for projects

--- src/components/profile-other-projects-panel/UserAppliedProjects.jsx
+++ src/components/profile-other-projects-panel/UserAppliedProjects.jsx
@@ -30,14 +30,18 @@ function UserAppliedProjects() {
       });
   }, []);
   let UserAppliedProjectsCards = [];
+  const baseProjectImage =
+    "https://cdn.pixabay.com/photo/2014/10/05/19/02/binary-code-475664_960_720.jpg";
+  const baseProjectLogo =
+    "https://cdn.pixabay.com/photo/2017/01/31/20/53/robot-2027195_960_720.png";
   if (UserAppliedProjects?.length) {
     UserAppliedProjectsCards = UserAppliedProjects?.map((project, idx) => {
       const projectCardDetails = {
-        projectImg: project.image_urls[0],
+        projectImg: project.image_urls[0] || baseProjectImage,
         title: project.title,
         tagline: project.tagline,
         logo: project.logo_url,
-        categories: project.categories,
+        categories: project.categories || baseProjectLogo,
         slug: project.slug,
       };
       return (
