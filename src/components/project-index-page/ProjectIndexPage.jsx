cd43c71c75efdb64ad9d3e93e229e25884b3e6b1
Zoe Nguyen <nganguyen0703@gmail.com>
2022-09-19 18:13:34 +0800

    feat: add space for filters on project index page

--- src/components/project-index-page/ProjectIndexPage.jsx
+++ src/components/project-index-page/ProjectIndexPage.jsx
@@ -6,6 +6,7 @@ import ProjectCard from "../cards/project-card/ProjectCard";
 import { useState, useEffect } from "react";
 import axios from "../../api/axios";
 import { Typography } from "@mui/material";
+import './ProjectIndexPage.scss';
 
 function ProjectIndexPage() {
   const [categories, setCategories] = useState([]);
@@ -14,16 +15,32 @@ function ProjectIndexPage() {
   }
   return (
     <>
-      <Typography
-        variant="h2"
-        textAlign={"center"}
-        marginY={4}
-        sx={{ color: "var(--color3)", fontWeight: "bold" }}
+      <Grid 
+        container
+        spacing={4}
+        columns={{ xs: 1, md: 12 }}
+        justifyContent="space-between"
+        marginTop={4}
       >
-        Projects
-      </Typography>
-      <Box></Box>
-      <ProjectIndexGrid getFilter={getFilter}/>
+        <Grid md={2} item className='filters'>
+          <Typography variant='h6' component='h2' color='white'>
+            Filters
+          </Typography>
+        </Grid>
+
+        <Grid md={10} sx={{ height: "100%" }} paddingTop={0} item border='1px solid green'>
+          <Typography
+            variant="h4"
+            component='h1'
+            textAlign={"center"}
+            marginY={4}
+            sx={{ color: "var(--color3)", fontWeight: "bold" }}
+          >
+            Projects
+          </Typography>
+          <ProjectIndexGrid getFilter={getFilter}/>
+        </Grid>
+      </Grid> 
     </>
   );
 }
