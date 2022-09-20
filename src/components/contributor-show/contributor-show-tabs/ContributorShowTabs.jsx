b05732349009ded66b583cdd4a0f526d355f97e8
Zoe Nguyen <nganguyen0703@gmail.com>
2022-09-20 21:09:27 +0800

    chore: add file for contributor show tabs

---
+++ src/components/contributor-show/contributor-show-tabs/ContributorShowTabs.jsx
@@ -0,0 +1,10 @@
+import Tabs, { tabsClasses } from "@mui/material/Tabs";
+
+import AuthContext from '../../../context/AuthProvider';
+
+export default function ContributorShowTabs() {
+
+  return (
+    <></>
+  )
+}
