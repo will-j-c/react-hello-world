d7114820e7af4a2bb6ccb3b761bc712c78621d02
MUI LE <tanmui1992@gmail.com>
2022-09-20 16:10:33 +0800

    feat(profile-edit): success setup edit profile test

--- src/components/profile-edit/ProfileEdit.jsx
+++ src/components/profile-edit/ProfileEdit.jsx
@@ -5,6 +5,14 @@ import TextField from "@mui/material/TextField";
 import Box from "@mui/material/Box";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Unstable_Grid2";
+import FormControlLabel from "@mui/material/FormControlLabel";
+import InputLabel from "@mui/material/InputLabel";
+import MenuItem from "@mui/material/MenuItem";
+import FormControl from "@mui/material/FormControl";
+import Select from "@mui/material/Select";
+import Chip from "@mui/material/Chip";
+import OutlinedInput from "@mui/material/OutlinedInput";
+import { useTheme } from "@mui/material/styles";
 
 import Button from "../buttons/Button";
 import styles from "./MultiForm.module.scss";
@@ -15,6 +23,7 @@ import AuthContext from "../../context/AuthProvider";
 import useAxiosPrivate from "../../hooks/useAxiosPrivate";
 
 function ProfileEdit() {
+  const theme = useTheme();
   const params = useParams();
   const username = params.username;
 
@@ -26,11 +35,40 @@ function ProfileEdit() {
   const isAuth = authUsername === username;
 
   const [skills, setSkills] = useState([]);
+  const [selectedSkills, setSelectedSkills] = useState([]);
   const [previewLogo, setPreviewLogo] = useState(null);
   const [previewProjectImages, setPreviewProjectImages] = useState([]);
   const [message, setMessage] = useState("");
-
   const [userData, setUserData] = useState({});
+
+  const ITEM_HEIGHT = 48;
+  const ITEM_PADDING_TOP = 8;
+
+  const MenuProps = {
+    PaperProps: {
+      style: {
+        maxHeight: ITEM_HEIGHT * 4.5 + ITEM_PADDING_TOP,
+        width: 250,
+      },
+    },
+  };
+  function getStyles(selection, selectionsList, theme) {
+    return {
+      fontWeight:
+        selectionsList.indexOf(selection) === -1
+          ? theme.typography.fontWeightRegular
+          : theme.typography.fontWeightMedium,
+      color:
+        selectionsList.indexOf(selection) === -1
+          ? "var(--color4)"
+          : "var(--color3)",
+    };
+  }
+
+  const skillsSelectChange = (event) => {
+    const { value } = event.target;
+    setSelectedSkills(typeof value === "string" ? value.split(",") : value);
+  };
 
   const [formData, setFormData] = useState({
     name: "",
@@ -44,15 +82,13 @@ function ProfileEdit() {
     profile_pic_url: "",
   });
   const formObj = {
-    name: useRef(),
-    tagline: useRef(),
-    interest: useRef(),
-    skills: useRef(),
-    linkedin: useRef(),
-    github: useRef(),
-    twitter: useRef(),
-    facebook: useRef(),
-    profile_pic_url: useRef(),
+    nameRef: useRef(),
+    taglineRef: useRef(),
+    interestRef: useRef(),
+    linkedinRef: useRef(),
+    githubRef: useRef(),
+    twitterRef: useRef(),
+    facebookRef: useRef(),
   };
 
   useEffect(() => {
@@ -62,222 +98,273 @@ function ProfileEdit() {
         const userResponse = await axios.get(`/users/${username}`);
         setSkills(validSkillList.data);
         setUserData(userResponse.data);
-        setFormData(userResponse.data);
-        // console.log("formData is: ", formData);
-        // console.log("formData name is: ", formData.name);
-        console.log("userData name is: ", userData);
+        // console.log("userData is: ", userData);
       } catch (error) {}
     }
     getData();
   }, [params]);
 
-  const handleFileInput = (event) => {
-    setFormData({
-      ...formData,
-      [event.target.name]: event.target.value,
-    });
-    console.log(formData);
-  };
+  const handleFileInput = (event) => {};
   const handleInputChange = (event) => {
     setFormData({
       ...formData,
       [event.target.name]: event.target.value,
     });
-    console.log(formData);
   };
-
+  // console.log("formObj.taglineRef is: ", formObj.interestRef);
   const handleFormSubmit = async (event) => {
     event.preventDefault();
-    // const title = formObj.titleRef.current.value;
-    // const city = formObj.cityRef?.current?.value || '';
-    // const description = formObj.descriptionRef.current.value;
-    // const remuneration = formObj.remunerationRef?.current?.value || '';
-    // const is_remote = isRemote;
-    // const commitment_level = commitment;
-    // const available_slots = availability;
-    // const skills = selectedSkills;
+    const name = formObj.nameRef.current.value;
+    const tagline = formObj.taglineRef.current.value;
+    const interest = formObj.interestRef.current.value;
+    // const linkedin = formObj.linkedinRef.current.value;
+    // const github = formObj.githubRef.current.value;
+    // const twitter = formObj.twitterRef.current.value;
+    // const facebook = formObj.facebookRef.current.value;
+
+    const skills = selectedSkills;
 
     try {
-      // const response = await axiosPrivate.post(
-      //     `/projects/${params.slug}/contributors`,
-      //     {
-      //       title,
-      //       is_remote,
-      //       commitment_level,
-      //       available_slots,
-      //       remuneration,
-      //       description,
-      //       city,
-      //       skills
-      //     }
-      //   )
-      //   setMessage(`Contributor successfully created. You will be redirected shortly...`);
-      //   setTimeout(navigate, 1500, `/contributors/${response.data._id}`);
+      const response = await axiosPrivate.put(`/users/${params.username}`, {
+        name,
+        tagline,
+        interest,
+        // linkedin,
+        // github,
+        // twitter,
+        // facebook,
+        skills,
+      });
+      console.log("success");
+      // navigate(`users/${authUsername}`);
+      setMessage(
+        `Contributor successfully created. You will be redirected shortly...`
+      );
+      setTimeout(navigate, 1500, `/users/${authUsername}`);
     } catch (error) {
-      // setMessage(error.response.data.error);
+      setMessage(error.response.data.error);
     }
   };
 
   return (
-    <Grid
-      sx={{
-        boxShadow: 7,
-        padding: 3,
-        backgroundColor: "var(--color1)",
-      }}
-      marginTop={4}
-      columns={{ xs: 1, md: 12 }}
-      container
-    >
-      <Grid md={4} item>
-        <Box display="flex" flexDirection={"column"} alignItems={"center"}>
-          <AvatarComponent
-            imgUrl={previewLogo}
-            sx={{
-              width: 128,
-              height: 128,
-              border: "solid 1px var(--color3)",
-              marginBottom: 2,
-            }}
-          />
-          <Button
-            variant="outlined"
-            title="Upload"
-            category="action"
-            upload={true}
-            // defaultValue={values.logo_url}
-            onChange={handleFileInput}
-          />
-        </Box>
-      </Grid>
-
-      <Grid md={8} item>
-        <Box
-          sx={{
-            padding: 3,
-            display: "flex",
-            flexDirection: "column",
-            alignItems: "center",
-            justifyContent: "center",
-          }}
-          className={styles["form"]}
-          marginTop={5}
+    <Box>
+      <Box sx={{ textAlign: "center" }}>
+        <Typography
+          variant="h4"
+          component="h1"
+          className="contributor-form-title"
         >
-          <form>
+          Update Profile information
+        </Typography>
+        <Typography
+          variant="body1"
+          component="h2"
+          className="contributor-form-subtitle"
+        >
+          We just need a few details, and you’ll be on your way.
+        </Typography>
+        {/* {message.length > 0 && (
+          <Box sx={{ marginTop: "1em" }}>
             <Typography
-              variant="subtitle1"
-              alignSelf={"flex-start"}
-              gutterBottom
+              variant={"caption"}
+              className={"contributor-form-message"}
             >
-              Name
+              {message}
             </Typography>
-            <TextField
-              required
-              hiddenLabel
-              // fullWidth
-              defaultValue={formData.name} //TODO: UPDATE THIS ONE LATER
-              onChange={handleInputChange}
-              variant="filled"
-              size="small"
-              type="text"
-              sx={{ marginBottom: 2 }}
-              className={styles["input-text"]}
-              placeholder="Filling your display name on your profile"
-              autoFocus
-            />
-            <Typography
-              variant="subtitle1"
-              alignSelf={"flex-start"}
-              gutterBottom
-            >
-              Headline
-            </Typography>
-            <TextField
-              required
-              hiddenLabel
-              fullWidth
-              // defaultValue="hehe" //TODO: UPDATE THIS ONE LATER
-              // onChange={handleChange("tagline")}
-              type="text"
-              variant="filled"
-              size="small"
-              sx={{ marginBottom: 2 }}
-              className={styles["input-text"]}
-              placeholder="A Short tagline describing yourself"
-            />
-            <Typography
-              variant="subtitle1"
-              alignSelf={"flex-start"}
-              gutterBottom
-            >
-              About
-            </Typography>
-            <TextField
-              required
-              hiddenLabel
-              fullWidth
-              // defaultValue="hehe" //TODO: UPDATE THIS ONE LATER
-              // onChange={handleChange("tagline")}
-              type="text"
-              variant="filled"
-              size="small"
-              sx={{ marginBottom: 2 }}
-              className={styles["input-text"]}
-              placeholder="A Short tagline describing yourself"
-              multiline
-            />
-            <Typography
-              variant="subtitle1"
-              alignSelf={"flex-start"}
-              gutterBottom
-            >
-              Interests
-            </Typography>
-            <TextField
-              required
-              hiddenLabel
-              fullWidth
-              // defaultValue="hehe"
-              // onChange={handleChange("tagline")}
-              type="text"
-              variant="filled"
-              size="small"
-              sx={{ marginBottom: 2 }}
-              className={styles["input-text"]}
-              placeholder="Add filling your interest"
-            />
-            <Box
-              display="flex"
-              height={0.3}
-              marginBottom={10}
-              marginTop={1}
-              alignSelf="flex-start"
-            >
-              haha hhooa Interest sadada
-            </Box>
-            {/* <Typography
-              variant="subtitle1"
-              alignSelf={"flex-start"}
-              gutterBottom
-            >
-              Skills
-            </Typography> */}
-
-            {/* SUBMIT BUTTON  */}
-            <Box textAlign={"center"} alignSelf={"flex-end"}>
-              {/* <Button variant="outlined" category="action" title="Cancel" /> */}
+          </Box>
+        )} */}
+      </Box>
+      <form onSubmit={handleFormSubmit}>
+        <Grid
+          sx={{
+            boxShadow: 7,
+            padding: 3,
+            backgroundColor: "var(--color1)",
+          }}
+          marginTop={4}
+          container
+        >
+          <Grid md={4} item>
+            <Box display="flex" flexDirection={"column"} alignItems={"center"}>
+              <AvatarComponent
+                imgUrl={previewLogo}
+                sx={{
+                  width: 128,
+                  height: 128,
+                  border: "solid 1px var(--color3)",
+                  marginBottom: 2,
+                }}
+              />
               <Button
                 variant="outlined"
-                title="Submit →"
+                title="Upload"
                 category="action"
-                isFullWidth={true}
-                onClick={handleFormSubmit}
+                upload={true}
+                // defaultValue={values.logo_url}
+                onChange={handleFileInput}
               />
             </Box>
-          </form>
-        </Box>
-      </Grid>
-    </Grid>
+          </Grid>
+
+          <Grid md={8} item>
+            <Box
+              sx={{
+                padding: 3,
+                display: "flex",
+                flexDirection: "column",
+                alignItems: "center",
+                justifyContent: "center",
+              }}
+              className={styles["form"]}
+              marginTop={5}
+            >
+              <Typography
+                variant="subtitle1"
+                alignSelf={"flex-start"}
+                gutterBottom
+              >
+                Name
+              </Typography>
+              <TextField
+                required
+                hiddenLabel
+                fullWidth
+                defaultValue={userData.name}
+                onChange={handleInputChange}
+                variant="filled"
+                size="small"
+                type="text"
+                sx={{ marginBottom: 2 }}
+                className={styles["input-text"]}
+                // className="input-text"
+                placeholder="Filling your display name on your profile"
+                autoFocus
+                inputRef={formObj.nameRef}
+              />
+
+              <Typography
+                variant="subtitle1"
+                alignSelf={"flex-start"}
+                gutterBottom
+              >
+                Headline
+              </Typography>
+              <TextField
+                required
+                hiddenLabel
+                fullWidth
+                defaultValue={userData.tagline}
+                onChange={handleInputChange}
+                type="text"
+                variant="filled"
+                size="small"
+                sx={{ marginBottom: 2 }}
+                className={styles["input-text"]}
+                placeholder="A Short tagline describing yourself"
+                inputRef={formObj.taglineRef}
+              />
+              <Typography
+                variant="subtitle1"
+                alignSelf={"flex-start"}
+                gutterBottom
+              >
+                About
+              </Typography>
+              <TextField
+                required
+                hiddenLabel
+                fullWidth
+                defaultValue={userData.about}
+                onChange={handleInputChange}
+                type="text"
+                variant="filled"
+                size="small"
+                sx={{ marginBottom: 2 }}
+                className={styles["input-text"]}
+                placeholder="A Short tagline describing yourself"
+                multiline
+                inputRef={formObj.aboutRef}
+              />
+              <Typography
+                variant="subtitle1"
+                alignSelf={"flex-start"}
+                gutterBottom
+              >
+                Interests
+              </Typography>
+              <TextField
+                required
+                hiddenLabel
+                fullWidth
+                defaultValue={userData.interest}
+                onChange={handleInputChange}
+                type="text"
+                variant="filled"
+                size="small"
+                sx={{ marginBottom: 2 }}
+                className={styles["input-text"]}
+                placeholder="Add filling your interest"
+                inputRef={formObj.interestRef}
+              />
+              <Box
+                display="flex"
+                height={0.3}
+                marginBottom={10}
+                marginTop={1}
+                alignSelf="flex-start"
+              >
+                haha hhooa Interest sadada
+              </Box>
+              <FormControl>
+                <InputLabel id="skills-multiple-chip-label">
+                  Select skills
+                </InputLabel>
+                <Select
+                  labelId="skills-multiple-chip-label"
+                  id="skills-multiple-chip"
+                  multiple
+                  value={selectedSkills}
+                  onChange={skillsSelectChange}
+                  input={
+                    <OutlinedInput id="select-multiple-chip" label="Chip" />
+                  }
+                  renderValue={(selected) => (
+                    <Box sx={{ display: "flex", flexWrap: "wrap", gap: 0.5 }}>
+                      {selected.map((value) => (
+                        <Chip key={value} label={value} />
+                      ))}
+                    </Box>
+                  )}
+                  MenuProps={MenuProps}
+                >
+                  {skills.map((skill) => (
+                    <MenuItem
+                      key={skill}
+                      value={skill}
+                      style={getStyles(skill, selectedSkills, theme)}
+                    >
+                      {skill}
+                    </MenuItem>
+                  ))}
+                </Select>
+              </FormControl>
+
+              {/* SUBMIT BUTTON  */}
+              <Box textAlign={"center"} alignSelf={"flex-end"}>
+                {/* <Button variant="outlined" category="action" title="Cancel" /> */}
+                <Button
+                  variant="outlined"
+                  title="Submit →"
+                  category="action"
+                  isFullWidth={true}
+                  onClick={handleFormSubmit}
+                />
+              </Box>
+            </Box>
+          </Grid>
+        </Grid>
+      </form>
+    </Box>
   );
 }
 
