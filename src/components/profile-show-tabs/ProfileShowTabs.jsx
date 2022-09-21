207d04045370b0ba8fd31f483d3b03e9dbfa592e
MUI LE <tanmui1992@gmail.com>
2022-09-21 20:39:01 +0800

    feat(profileshowtabs): styling Tabs panel in profile page

--- src/components/profile-show-tabs/ProfileShowTabs.jsx
+++ src/components/profile-show-tabs/ProfileShowTabs.jsx
@@ -2,9 +2,42 @@ import { useParams } from "react-router-dom";
 import Tabs, { tabsClasses } from "@mui/material/Tabs";
 import { useState, useEffect, useContext } from "react";
 import Tab from "@mui/material/Tab";
-import axios from "../../api/axios";
-import AuthContext from "../../context/AuthProvider";
-import useAxiosPrivate from "../../hooks/useAxiosPrivate";
+
+import { createTheme } from "@mui/material/styles";
+import { ThemeProvider } from "@mui/material/styles";
+import { NoEncryption } from "@mui/icons-material";
+
+const themeTab = createTheme({
+  components: {
+    MuiTabs: {
+      styleOverrides: {
+        indicator: {
+          display: "none",
+        },
+      },
+    },
+    MuiTab: {
+      styleOverrides: {
+        root: {
+          height: 0,
+          paddingTop: 1,
+          paddingBottom: 1,
+          marginRight: 10,
+          borderRadius: 5,
+          backgroundColor: "var(--color1)",
+          color: "var(--color4)",
+          minHeight: 38,
+          
+          "&.Mui-selected": {
+            backgroundColor: "var(--color3)",
+            fontWeight: "bold",
+            color: "var(--color1)",
+          },
+        },
+      },
+    },
+  },
+});
 
 function ProfileShowTabs(props) {
   const isAuth = props.isAuth;
@@ -16,11 +49,6 @@ function ProfileShowTabs(props) {
       scrollButtons
       allowScrollButtonsMobile
       aria-label="scrollable force tabs example"
-      // sx={{
-      //   [`& .${tabsClasses.scrollButtons}`]: {
-      //     "&.Mui-disabled": { opacity: 0.7, color: "white" },
-      //   },
-      // }}
     >
       <Tab value="1" label="Profile" />
       <Tab value="2" label="Connection" />
@@ -31,24 +59,27 @@ function ProfileShowTabs(props) {
 }
 function ShowTabsConnection(props) {
   return (
+    <ThemeProvider theme={themeTab}>
     <Tabs value={props.tabValue} onChange={props.handleTabChange}>
       <Tab
         value="1"
         label="Following"
         className="small-tab"
-        // sx={{ backgroundColor: "white", color: "red" }}
       />
       <Tab value="2" label="Followers" className="small-tab" />
     </Tabs>
+    </ThemeProvider>
   );
 }
 function ShowTabsOtherProjects(props) {
   return (
-    <Tabs value={props.tabValue} onChange={props.handleTabChange}>
-      <Tab value="1" label="Working on" />
-      <Tab value="2" label="Applied projects" />
-      <Tab value="3" label="Following projects" />
-    </Tabs>
+    <ThemeProvider theme={themeTab}>
+      <Tabs value={props.tabValue} onChange={props.handleTabChange}>
+        <Tab value="1" label="Working on" />
+        <Tab value="2" label="Applied projects" />
+        <Tab value="3" label="Following projects" />
+      </Tabs>
+    </ThemeProvider>
   );
 }
 
